e5fb1c519f9045aade792bb0168c1f7f7c5556ee
vinnychi123 <irishspring243@gmail.com>
2026-01-03 16:24:08 -0500

    initial setup

---
+++ include/ring_buffer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <atomic>
+#include <cstddef>
+
+
+template <typename T, std::size_t Capacity>
+class RingBuffer{
+    static_assert((Capacity & (Capacity - 1)) == 0, "Capacity must be a power of two");
+
+    public:
+        RingBuffer(): head_(0), tail_(0) {}
+        
+        bool try_push(const T& item) {
+            size_t cur_head = head.load(std::memory_order_seq_cst);
+            
+        };
+        bool try_pop(T& item);
+        bool is_empty() const {
+            return head_.load(std::memory_order_seq_cst) == tail_.load(std::memory_order_seq_cst);
+        };
+        bool is_full() const {
+            return ((head_.load(std::memory_order_seq_cst) - tail_.load(std::memory_order_seq_cst)) & mask) == mask;
+        };
+    
+    private:
+        static constexpr size_t mask = Capacity - 1;
+        alignas(64) std::atomic<size_t> head_{0};
+        alignas(64) std::atomic<size_t> tail_{0};
+        T buffer_[Capacity];
+};
