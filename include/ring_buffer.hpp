c18d439c69ae257bf3e22dcda2ecb2a72c703407
vinnychi123 <irishspring243@gmail.com>
2026-01-03 16:53:03 -0500

    implemented ring buffer

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -13,30 +13,37 @@ class RingBuffer{
         bool try_push(const T& item) {
             size_t cur_head = head_.load(std::memory_order_seq_cst);
             size_t cur_tail = tail_.load(std::memory_order_seq_cst);
+
             if (((cur_head - cur_tail) & mask) == mask) {
                 return false; 
             }
+
             buffer_[cur_head & mask] = item;
             head_.store(cur_head + 1, std::memory_order_seq_cst);
+
             return true;
 
-        };
+        }
         bool try_pop(T& item){
             size_t cur_tail = tail_.load(std::memory_order_seq_cst);
-            if (cur_tail == head_.load(std::memory_order_seq_cst)) {
+            size_t cur_head = head_.load(std::memory_order_seq_cst);
+
+            if (cur_tail == cur_head) {
                 return false; // Buffer is empty
             }
-            cur_tail = tail_.fetch_add(1, std::memory_order_seq_cst);  // returns old value, then adds 1
+
             item = buffer_[cur_tail & mask];
+            tail_.store(cur_tail + 1, std::memory_order_seq_cst);
+
             return true;
            
-        };
+        }
         bool is_empty() const {
             return head_.load(std::memory_order_seq_cst) == tail_.load(std::memory_order_seq_cst);
-        };
+        }
         bool is_full() const {
             return ((head_.load(std::memory_order_seq_cst) - tail_.load(std::memory_order_seq_cst)) & mask) == mask;
-        };
+        }
     
     private:
         static constexpr size_t mask = Capacity - 1;
